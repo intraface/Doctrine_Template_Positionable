c0402296adb34d5b051dd210486d965836a3da77
lsolesen <lars@intraface.dk>
2016-04-20 08:47:35 +0000

    Use vendor autoloader

--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'Doctrine.php';
-
-set_include_path(realpath(dirname(__FILE__) . '/../src/') . PATH_SEPARATOR . get_include_path());
-
-spl_autoload_register(array('Doctrine', 'autoload'));
-
